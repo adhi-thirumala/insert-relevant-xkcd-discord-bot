d9282ecf8f6b20fcced98461734750f098133d59
adhi <me@adhithirumala.com>
2025-11-24 21:35:34 -0500

    added doc comments to models structs

--- db/src/models.rs
+++ db/src/models.rs
@@ -1,6 +1,25 @@
 use serde::{Deserialize, Serialize};
 use strum::{Display, EnumString};
 
+/// Represents a full comic that has been scraped.
+///
+/// This struct contains all the information about a comic, including its number, title, URL, and other metadata.
+///
+///
+/// # Example
+/// ```
+/// let comics = Comics {
+///    comic_number: 1,
+///    title: "Title".to_string(),
+///    url: "https://example.com".to_string(),
+///    xkcd_url: "https://xkcd.com".to_string(),
+///    hover_text: Some("Hover Text".to_string()),
+///    last_revision_id: 1,
+///    last_revision_timestamp: "2023-01-01T00:00:00Z".to_string(),
+///    scraped_at: "2023-01-01T00:00:00Z".to_string(),
+///    updated_at: "2023-01-01T00:00:00Z".to_string(),
+///};
+/// ```
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Comics {
   pub comic_number: u64,
@@ -14,6 +33,10 @@ pub struct Comics {
   pub updated_at: String,
 }
 
+/// Represents the type of section in a comic.
+///
+/// This enum defines the different types of sections that can be found in a comic. Supports direct conversion to and from string via the `Display` and `FromStr` traits.
+///
 #[derive(Debug, Clone, Serialize, Deserialize, Display, EnumString)]
 #[serde(rename_all = "snake_case")]
 #[strum(serialize_all = "snake_case")]
@@ -25,6 +48,22 @@ pub enum SectionType {
   Other,
 }
 
+/// Represents a chunk of text from a comic.
+///
+/// This struct contains all the information about a chunk of text, including its ID, comic number, text, index, section type, and embedding.
+///
+///
+/// # Example
+/// ```
+/// let chunks = Chunks {
+///    id: 1,
+///    comic_number: 1,
+///    chunk_text: "Chunk Text".to_string(),
+///    chunk_index: 1,
+///    section_type: Some(SectionType::TitleHover),
+///    embedding: vec![0.0, 0.0, 0.0],
+///};
+/// ```
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Chunks {
   pub id: u64,
